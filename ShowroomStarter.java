93ce9b7e6a06518e8fb63ad18ea952ab566a957d
ambikat27 <149304109+ambikat27@users.noreply.github.com>
2023-10-31 12:05:16 +0530

    Add files via upload

---
+++ ShowroomStarter.java
@@ -0,0 +1,110 @@
+class ShowroomStarter{
+
+ public static void main(String[] args)
+ {
+  System.out.println("Starting of ShowroomStarter");
+  Showroom showroom=new Showroom();
+  
+  System.out.println("showroom name:"+showroom.name);
+  System.out.println("showroom location:"+showroom.location);
+  System.out.println("showroom landmark:"+showroom.landmark);
+  System.out.println("showroom pincode:"+showroom.pincode);
+  System.out.println("showroom noOfWorkers:"+showroom.noOfWorkers);
+  System.out.println("showroom ownername:"+showroom.ownername);
+  System.out.println("showroom products:"+showroom.products);
+  System.out.println("showroom sevices:"+showroom.services);
+   
+  Showroom showroom1=new Showroom("TATA");
+  
+  System.out.println("showroom name:"+showroom1.name);
+  System.out.println("showroom location:"+showroom1.location);
+  System.out.println("showroom landmark:"+showroom1.landmark);
+  System.out.println("showroom pincode:"+showroom1.pincode);
+  System.out.println("showroom noOfWorkers:"+showroom1.noOfWorkers);
+  System.out.println("showroom ownername:"+showroom1.ownername);
+  System.out.println("showroom products:"+showroom1.products);
+  System.out.println("showroom sevices:"+showroom1.services);
+  
+  Showroom showroom2=new Showroom("TATA","banglore");
+  
+  System.out.println("showroom name:"+showroom2.name);
+  System.out.println("showroom location:"+showroom2.location);
+  System.out.println("showroom landmark:"+showroom2.landmark);
+  System.out.println("showroom pincode:"+showroom2.pincode);
+  System.out.println("showroom noOfWorkers:"+showroom2.noOfWorkers);
+  System.out.println("showroom ownername:"+showroom2.ownername);
+  System.out.println("showroom products:"+showroom2.products);
+  System.out.println("showroom sevices:"+showroom2.services);
+  
+  Showroom showroom3=new Showroom("TATA","banglore","Near Dmark");
+  
+  System.out.println("showroom name:"+showroom3.name);
+  System.out.println("showroom location:"+showroom3.location);
+  System.out.println("showroom landmark:"+showroom3.landmark);
+  System.out.println("showroom pincode:"+showroom3.pincode);
+  System.out.println("showroom noOfWorkers:"+showroom3.noOfWorkers);
+  System.out.println("showroom ownername:"+showroom3.ownername);
+  System.out.println("showroom products:"+showroom3.products);
+  System.out.println("showroom sevices:"+showroom3.services);
+   
+  Showroom showroom8=new Showroom("TATA","banglore","Near Dmark",577589);
+  
+  System.out.println("showroom name:"+showroom8.name);
+  System.out.println("showroom location:"+showroom8.location);
+  System.out.println("showroom landmark:"+showroom8.landmark);
+  System.out.println("showroom pincode:"+showroom8.pincode);
+  System.out.println("showroom noOfWorkers:"+showroom8.noOfWorkers);
+  System.out.println("showroom ownername:"+showroom8.ownername);
+  System.out.println("showroom products:"+showroom8.products);
+  System.out.println("showroom sevices:"+showroom8.services);
+   
+  Showroom showroom4=new Showroom("TATA","banglore","Near Dmark",577589,48);
+  
+  System.out.println("showroom name:"+showroom4.name);
+  System.out.println("showroom location:"+showroom4.location);
+  System.out.println("showroom landmark:"+showroom4.landmark);
+  System.out.println("showroom pincode:"+showroom4.pincode);
+  System.out.println("showroom noOfWorkers:"+showroom4.noOfWorkers);
+  System.out.println("showroom ownername:"+showroom4.ownername);
+  System.out.println("showroom products:"+showroom4.products);
+  System.out.println("showroom sevices:"+showroom4.services);
+   
+  Showroom showroom5=new Showroom("TATA","banglore","Near Dmark",577589,48,"rathan");
+  
+  System.out.println("showroom name:"+showroom5.name);
+  System.out.println("showroom location:"+showroom5.location);
+  System.out.println("showroom landmark:"+showroom5.landmark);
+  System.out.println("showroom pincode:"+showroom5.pincode);
+  System.out.println("showroom noOfWorkers:"+showroom5.noOfWorkers);
+  System.out.println("showroom ownername:"+showroom5.ownername);
+  System.out.println("showroom products:"+showroom5.products);
+  System.out.println("showroom sevices:"+showroom5.services);
+   
+  Showroom showroom6=new Showroom("TATA","banglore","Near Dmark",577589,48,"rathan","car");
+  
+  System.out.println("showroom name:"+showroom6.name);
+  System.out.println("showroom location:"+showroom6.location);
+  System.out.println("showroom landmark:"+showroom6.landmark);
+  System.out.println("showroom pincode:"+showroom6.pincode);
+  System.out.println("showroom noOfWorkers:"+showroom6.noOfWorkers);
+  System.out.println("showroom ownername:"+showroom6.ownername);
+  System.out.println("showroom products:"+showroom6.products);
+  System.out.println("showroom sevices:"+showroom6.services);
+   
+  Showroom showroom7=new Showroom("TATA","banglore","Near Dmark",577589,48,"rathan","car","cleaning");
+
+  System.out.println("showroom name:"+showroom7.name);
+  System.out.println("showroom location:"+showroom7.location);
+  System.out.println("showroom landmark:"+showroom7.landmark);
+  System.out.println("showroom pincode:"+showroom7.pincode);
+  System.out.println("showroom noOfWorkers:"+showroom7.noOfWorkers);
+  System.out.println("showroom ownername:"+showroom7.ownername);
+  System.out.println("showroom products:"+showroom7.products);
+  System.out.println("showroom sevices:"+showroom7.services);  
+  
+  System.out.println("Ending of ShowroomStarter");
+ }
+
+
+
+}
