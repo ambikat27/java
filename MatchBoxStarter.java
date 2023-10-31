5b38eb79897ad015063d91805c794f9b44fc8236
ambikat27 <149304109+ambikat27@users.noreply.github.com>
2023-10-31 12:02:47 +0530

    Add files via upload

---
+++ MatchBoxStarter.java
@@ -0,0 +1,89 @@
+class MatchBoxStarter{
+   public static void main(String[] args)
+   {
+   System.out.println("Starting of Matchboxstarter ");
+   
+   MatchBox matchbox=new MatchBox();
+   
+   System.out.println("matchbox brand is:"+matchbox.brand);
+   System.out.println("matchbox noOfSticks is:"+matchbox.noOfSticks);
+   System.out.println("matchbox length is:"+matchbox.length);
+   System.out.println("matchbox price is:"+matchbox.price);
+   System.out.println("matchbox size is:"+matchbox.size); 
+   System.out.println("matchbox weight is:"+matchbox.weight);
+   System.out.println("matchbox type is:"+matchbox.type); 
+   
+   MatchBox matchbox1=new MatchBox("swarna");
+   
+   System.out.println("matchbox brand is:"+matchbox1.brand);
+   System.out.println("matchbox noOfSticks is:"+matchbox1.noOfSticks);
+   System.out.println("matchbox length is:"+matchbox1.length);
+   System.out.println("matchbox price is:"+matchbox1.price);
+   System.out.println("matchbox size is:"+matchbox1.size); 
+   System.out.println("matchbox weight   is:"+matchbox1.weight);
+   System.out.println("matchbox type is:"+matchbox1.type); 
+   
+   MatchBox matchbox2=new MatchBox("mattle",23);
+   
+   System.out.println("matchbox brand is:"+matchbox2.brand);
+   System.out.println("matchbox noOfSticks is:"+matchbox2.noOfSticks);
+   System.out.println("matchbox length is:"+matchbox2.length);
+   System.out.println("matchbox price is:"+matchbox2.price);
+   System.out.println("matchbox size is:"+matchbox2.size); 
+   System.out.println("matchbox weight is:"+matchbox2.weight);
+   System.out.println("matchbox type is:"+matchbox2.type); 
+   
+   MatchBox matchbox3=new MatchBox("mattle",23,1.0);
+   
+   System.out.println("matchbox brand is:"+matchbox3.brand);
+   System.out.println("matchbox noOfSticks is:"+matchbox3.noOfSticks);
+   System.out.println("matchbox length is:"+matchbox3.length);
+   System.out.println("matchbox price is:"+matchbox3.price);
+   System.out.println("matchbox size is:"+matchbox3.size); 
+   System.out.println("matchbox weight is:"+matchbox3.weight);
+   System.out.println("matchbox type is:"+matchbox3.type); 
+   
+   MatchBox matchbox4=new MatchBox("mattle",23,1,2);
+   
+   System.out.println("matchbox brand is:"+matchbox4.brand);
+   System.out.println("matchbox noOfSticks is:"+matchbox4.noOfSticks);
+   System.out.println("matchbox length is:"+matchbox4.length);
+   System.out.println("matchbox price is:"+matchbox4.price);
+   System.out.println("matchbox size is:"+matchbox4.size); 
+   System.out.println("matchbox weight is:"+matchbox4.weight);
+   System.out.println("matchbox type is:"+matchbox4.type); 
+   
+   MatchBox matchbox5=new MatchBox("mattle",23,1,2,'s');
+   
+   System.out.println("matchbox brand is:"+matchbox5.brand);
+   System.out.println("matchbox noofSticks is:"+matchbox5.noOfSticks);
+   System.out.println("matchbox length is:"+matchbox5.length);
+   System.out.println("matchbox price is:"+matchbox5.price);
+   System.out.println("matchbox size is:"+matchbox5.size); 
+   System.out.println("matchbox weight is:"+matchbox5.weight);
+   System.out.println("matchbox type is:"+matchbox5.type); 
+   
+   MatchBox matchbox6=new MatchBox("mattle",23,1.0,2,'s',1);
+   
+   System.out.println("matchbox brand is:"+matchbox6.brand);
+   System.out.println("matchbox noOfSticks is:"+matchbox6.noOfSticks);
+   System.out.println("matchbox length is:"+matchbox6.length);
+   System.out.println("matchbox price is:"+matchbox6.price);
+   System.out.println("matchbox size is:"+matchbox6.size); 
+   System.out.println("matchbox weight is:"+matchbox6.weight);
+   System.out.println("matchbox type is:"+matchbox6.type); 
+   
+   MatchBox matchbox7=new MatchBox("mattle",23,1.0,2,'s',1,"wood");
+   
+   System.out.println("matchbox brand is:"+matchbox7.brand);
+   System.out.println("matchbox noOfSticks is:"+matchbox7.noOfSticks);
+   System.out.println("matchbox length is:"+matchbox7.length);
+   System.out.println("matchbox price is:"+matchbox7.price);
+   System.out.println("matchbox size is:"+matchbox7.size); 
+   System.out.println("matchbox weight is:"+matchbox7.weight);
+   System.out.println("matchbox type is:"+matchbox7.type); 
+   
+  System.out.println("Starting of Matchboxstarter ");
+   }
+
+}
